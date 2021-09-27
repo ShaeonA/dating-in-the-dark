b00392d8b1598d591c6860369679517de92cc16b
Shaeon <Shaeonada@gmail.com>
2021-09-27 14:26:52 -0400

    updated server/installed packages for back end

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/dating-in-the-dark', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
