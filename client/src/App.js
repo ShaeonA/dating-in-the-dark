f2eb5ed5dad1c306dceca041f3cc86a5e6a5062b
kerbyoboy <kerbyhenry75@icloud.com>
2021-10-04 19:00:32 -0400

    added css but button isn't functional

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="logo1.png" className="" alt="logo" />
+        <h1>
+        </h1>
+        <button type="button" id="login">Login</button>
+        <button type="button" id="signUp">Sign Up</button>
       </header>
     </div>
   );
