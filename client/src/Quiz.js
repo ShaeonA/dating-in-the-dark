c65a1c44cf15e66addd2f31e926fb40c8a916ca6
Ashley Thomas <thomas.ashley.r@gmail.com>
2021-10-04 19:20:32 -0400

    Adding Quiz

---
+++ client/src/Quiz.js
@@ -0,0 +1,62 @@
+export const quiz = {
+    "quizTitle": "What's Your Personality Type",
+    "quizSynopsis": "you will be matched based on their Enneagram Personality type.",
+    "questions": [
+        {
+            "question": "I've been...",
+            "questionType": "text",
+            "answerSelectionType": "single",
+            "answers": [
+                "A. romantic and imaginative B. pragmatic and down to earth"
+            ],
+            "correctAnswer": [A, B],
+            "ForCorrectAnswer": "Great! Keep going!",
+            "point": [1, 2]
+        },
+        {
+            "question": "I have tended to...",
+            "questionType": "text",
+            "answerSelectionType": "single",
+            "answers": [
+                "A. take on confrontations B. avoid confrontations"
+            ],
+            "correctAnswer": [A, B],
+            "ForCorrectAnswer": "It's cuffing season...",
+            "point": [1, 2]
+        },
+        {
+            "question": "I have typically been...",
+            "questionType": "text",
+            "answerSelectionType": "single",
+            "answers": [
+                "A. diplomatic, charming, and ambitious B. direct, formal, and idealistic"
+            ],
+            "correctAnswer": [A, B],
+            "ForCorrectAnswer": "Great! Keep going!",
+            "point": [1, 2]
+        },
+        {
+            "question": "I have tended to be...",
+            "questionType": "text",
+            "answerSelectionType": "single",
+            "answers": [
+                "A. focused and intense B. pspontaneous and fun-loving"
+            ],
+            "correctAnswer": [A, B],
+            "ForCorrectAnswer": "One more question!",
+            "point": [1, 2]
+        },
+        {
+            "question": "I have been a...",
+            "questionType": "text",
+            "answerSelectionType": "single",
+            "answers": [
+                "A. hospitable person and have enjoyed welcoming new friends into my life B. private person and have not mixed much with others"
+            ],
+            "correctAnswer": [A, B],
+            "ForCorrectAnswer": "Get ready for that date!",
+            "point": [1, 2]
+        },
+    ]
+}
+
